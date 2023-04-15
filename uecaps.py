1488c5c386618ff94fc6fd5f5e9b7170e609972a
nxij <tanel463@hotmail.com>
2023-04-15 14:19:08 +0300

    Cleaned and commented the script

--- uecaps.py
+++ uecaps.py
@@ -4,17 +4,23 @@ import sys
 import re
 import string
 
+# Load .binarypb file given as an argument
 with open(sys.argv[1], mode='rb') as file:
     fileContent = file.read()
 
 server = uecaps_pb2.a()
 server.ParseFromString(fileContent)
 
+# While decompiling the protobuf, the NR band get a leading group of numbers in the form of "100"
+# So for example the band n78 looks like "10078"
+# LTE bands do not have these leading numbers
+# Here we exclude LTE bands and assign an "n" infront of the NR band
 def bandhandler(band):
     if band > 300:
         band = re.sub("^10+(?!$)", "n", str(band))
     return band
 
+# Attach the carrier count to the band number as an alphabetic letter
 def dlcombohandler(band, dlcc):
     dlcc = dlcc - 1
     if dlcc >= 2:
@@ -24,6 +30,8 @@ def dlcombohandler(band, dlcc):
     band = str(bandhandler(band)) + str(dlcc)
     return band
 
+# The band is not used for uplink if the carrier count is 0
+# Attach the carrier count to the band number as an alphabetic letter
 def ulcombohandler(band, ulcc):
     if ulcc > 0:
         if ulcc > 1:
@@ -33,6 +41,7 @@ def ulcombohandler(band, ulcc):
         band = str(bandhandler(band)) + str(ulcc)
         return band
 
+# Construct a list for both downlink and uplink combos
 def comboconstructor(combo):
     dlist, ulist = [], []
     for value2 in combo.c:
@@ -41,12 +50,9 @@ def comboconstructor(combo):
             ulist.append(ulcombohandler(value2.band, value2.ulcc))
     return dlist, ulist
 
-
+# Here I formated the output as a series of strings in the format that is usually found online (cacombos.com etc)
 for value in server.a:
     for value1 in value.b:
         dlc = [''.join([('_' if re.match("n.*",i) else '-')+i for i in comboconstructor(value1)[0]])]
         ulc = [''.join([('_' if re.match("n.*",i) else '-')+i for i in comboconstructor(value1)[1]])]
-        #nrc = 
         print(dlc[0][1:] + ',' + ulc[0][1:])
-        #print('_'.join(comboconstructor(value1)[0]))
-        #print('-'.join(['_'.join(comboconstructor(value1)[0])]))
