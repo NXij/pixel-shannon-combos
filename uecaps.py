d6203704ac4df9f0faca40e6593a138ebc2131e1
nxij <tanel463@hotmail.com>
2023-05-30 19:59:41 +0300

    Made some improvements in the meanwhile, restructured the script abit etc

--- uecaps.py
+++ uecaps.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 import uecaps_pb2 as uecaps_pb2
-import sys
 import re
 import string
-import struct
+import csv
+import os
+import argparse
+import io
+import time
 
-# Load .binarypb file given as an argument
-with open(sys.argv[1], mode='rb') as file:
-    fileContent = file.read()
+parser = argparse.ArgumentParser(description='Dump uecaps')
+parser.add_argument('input_dir', type=str, help="Provide the uecaps files directory")
+parser.add_argument('output_dir', type=str, help="Provide the output directory")
+args = parser.parse_args()
 
-server = uecaps_pb2.a()
-server.ParseFromString(fileContent)
-
-# While decompiling the protobuf, the NR band get a leading group of numbers in the form of "100"
+# While decompiling the protobuf, the NR band gets a leading group of numbers in the form of "100"
 # So for example the band n78 looks like "10078"
 # LTE bands do not have these leading numbers
 # Here we exclude LTE bands and assign an "n" infront of the NR band
@@ -64,22 +65,32 @@ def featuresetcontructor(fs, param):
 
 # Construct lists for both downlink and uplink values
 def comboconstructor(combo):
-    dlist, ulist, fsdl, fsul, bwdl, bwul, fsadl, fsbdl, fscdl, fsaul, fsbul, fscul = [], [], [], [], [], [], [], [], [], [], [], []
+    dlist = [dlcombohandler(value2.band, value2.dlcc) for value2 in combo.c]
+    ulist = [ulcombohandler(value2.band, value2.ulcc) for value2 in combo.c if ulcombohandler(value2.band, value2.ulcc) is not None]
+
+    fsdl, fsul, bwdl, bwul, fsadl, fsbdl, fscdl, fsaul, fsbul, fscul = ([] for i in range(10))
+
     for value2 in combo.c:
-        dlist.append(dlcombohandler(value2.band, value2.dlcc))
-        if ulcombohandler(value2.band, value2.ulcc) != None:
-            ulist.append(ulcombohandler(value2.band, value2.ulcc))
-        if featuresetcontructor(value2.fsdl, 0) != None:
-            fsdl.extend(featuresetcontructor(value2.fsdl, 0)[0]), bwdl.extend(featuresetcontructor(value2.fsdl, 0)[1]), fsadl.extend(featuresetcontructor(value2.fsdl, 0)[2])
-            fsbdl.extend(featuresetcontructor(value2.fsdl, 0)[3]), fscdl.extend(featuresetcontructor(value2.fsdl, 0)[4])
-        if featuresetcontructor(value2.fsul, 1) != None:
-            fsul.extend(featuresetcontructor(value2.fsul, 1)[0]), bwul.extend(featuresetcontructor(value2.fsul, 1)[1]), fsaul.extend(featuresetcontructor(value2.fsul, 1)[2])
-            fsbul.extend(featuresetcontructor(value2.fsul, 1)[3]), fscul.extend(featuresetcontructor(value2.fsul, 1)[4])
+        fs_dl = featuresetcontructor(value2.fsdl, 0)
+        if fs_dl:
+            fsdl += fs_dl[0]
+            bwdl += fs_dl[1]
+            fsadl += fs_dl[2]
+            fsbdl += fs_dl[3]
+            fscdl += fs_dl[4]
+        fs_ul = featuresetcontructor(value2.fsul, 1)
+        if fs_ul:
+            fsul += fs_ul[0]
+            bwul += fs_ul[1]
+            fsaul += fs_ul[2]
+            fsbul += fs_ul[3]
+            fscul += fs_ul[4]
+
     return dlist, ulist, fsdl, fsul, bwdl, bwul, fsadl, fsbdl, fscdl, fsaul, fsbul, fscul
 
 # Header
-print("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s" % 
-    ("DL comb", 
+header = [
+    "DL comb", 
     "DL MIMO", 
     "DL BW", 
     "DL Unk A",  # 4 = mmWave only, 2 = n3, n7, n38, n78, 1 = n1, n7, n8 ,n20, n28
@@ -90,37 +101,46 @@ print("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s" %
     "UL BW", 
     "UL Unk A", 
     "UL Unk B", # 1 = mmWave, 2 = regular
-    "UL Unk C")) # 1 = HPUE?, 0 = regular
+    "UL Unk C"] # 1 = HPUE?, 0 = regular
 
 # Construct the output
-for value in server.a:
-    for value1 in value.b:
-        dlc = [''.join([('_' if re.match("n.*",i) else '-')+i for i in comboconstructor(value1)[0]])]
-        ulc = [''.join([('_' if re.match("n.*",i) else '-')+i for i in comboconstructor(value1)[1]])]
-        print("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s" % 
-            # band dl combo
-            (dlc[0][1:], 
-            # band dl mimo
-            ';'.join(map(str, comboconstructor(value1)[2])),
-            # band dl bw
-            ';'.join(map(str, comboconstructor(value1)[4])),
-            # band dl unknown a
-            ';'.join(map(str, comboconstructor(value1)[6])),
-            # band dl unknown b
-            ';'.join(map(str, comboconstructor(value1)[7])),
-            # band dl unknown c
-            ';'.join(map(str, comboconstructor(value1)[8])),
-            # band ul combo
-            ulc[0][1:], 
-            # band ul mimo
-            ';'.join(map(str, comboconstructor(value1)[3])),
-            # band ul bw
-            ';'.join(map(str, comboconstructor(value1)[5])),
-            # band ul unknown a
-            ';'.join(map(str, comboconstructor(value1)[9])),
-            # band ul unknown b
-            ';'.join(map(str, comboconstructor(value1)[10])),
-            # band ul unknown a
-            ';'.join(map(str, comboconstructor(value1)[11]))))
-        #print(comboconstructor(value1)[0], ";", comboconstructor(value1)[2], sum(comboconstructor(value1)[2]), ";", comboconstructor(value1)[1],";", comboconstructor(value1)[3], sum(comboconstructor(value1)[3]))
-#comboconstructor(value1)[6], comboconstructor(value1)[7], comboconstructor(value1)[8], comboconstructor(value1)[9], comboconstructor(value1)[10], comboconstructor(value1)[11]
+def format_output(value1):
+    dlist, ulist, fsdl, fsul, bwdl, bwul, fsadl, fsbdl, fscdl, fsaul, fsbul, fscul = comboconstructor(value1)
+    dlc = ''.join([('_' if re.search("n.*", i) else '-') + i for i in dlist])
+    ulc = ''.join([('_' if re.match("n.*", i) else '-') + i for i in ulist])
+    return (dlc[1:], 
+            ';'.join(map(str, fsdl)),
+            ';'.join(map(str, bwdl)),
+            ';'.join(map(str, fsadl)),
+            ';'.join(map(str, fsbdl)),
+            ';'.join(map(str, fscdl)),
+            ulc[1:], 
+            ';'.join(map(str, fsul)),
+            ';'.join(map(str, bwul)),
+            ';'.join(map(str, fsaul)),
+            ';'.join(map(str, fsbul)),
+            ';'.join(map(str, fscul)))
+
+
+unixtime = int(time.time())
+os.makedirs(os.path.join(args.output_dir, f"dec_{unixtime}"), exist_ok=True)
+
+i = 0
+for entry in os.scandir(args.input_dir):
+    if entry.is_file() and entry.name.endswith('.binarypb'):
+        input_path = entry.path
+        with open(input_path, mode='rb') as file:
+            fileContent = file.read()
+            server = uecaps_pb2.a()
+            server.ParseFromString(fileContent)
+            output_path = os.path.join(args.output_dir, f"dec_{unixtime}", entry.name) + ".csv"
+            i += 1
+            print('\rFile: {}'.format(i), end='')
+            with io.StringIO(newline='') as output_buffer:
+                writer = csv.writer(output_buffer)
+                writer.writerow(header)
+                for value in server.a:
+                    for value1 in value.b:
+                        writer.writerow(format_output(value1))
+                with open(output_path, 'w', newline='') as output_file:
+                    output_file.write(output_buffer.getvalue())
